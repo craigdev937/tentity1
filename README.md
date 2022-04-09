7434da968e86efd019d3f7599549ac45ce19ede6
Craig <indigomx9@gmail.com>
2022-04-09 00:20:39 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# createEntityAdapter
+
+Inital setup of the project.  Created with TypeScript, React, Redux-Toolkit featuring createEntityAdapter.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nesciunt aliquam suscipit necessitatibus magnam enim inventore expedita deleniti at possimus sint officiis, sunt eligendi velit sapiente amet sequi error unde.</p>
+        </React.Fragment>
+    );
+};
+
+
